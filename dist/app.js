8adb74cc1f0d449a5cad37983449f93b2438c761
Akash G <dragon.abhi8@gmail.com>
2022-08-20 23:18:10 +0530

    updated

--- dist/app.js
+++ dist/app.js
@@ -169,9 +169,17 @@ class PrjectItem extends Component {
         this.renderContent();
         this.configure();
     }
+    get persons() {
+        if (this.project.people === 1) {
+            return "1 person";
+        }
+        else {
+            return `${this.project.people} persons`;
+        }
+    }
     renderContent() {
         this.element.querySelector('h2').textContent = this.project.title;
-        this.element.querySelector('h3').textContent = this.project.people.toString();
+        this.element.querySelector('h3').textContent = this.persons + " were assigned";
         this.element.querySelector('p').textContent = this.project.description;
     }
     configure() {
