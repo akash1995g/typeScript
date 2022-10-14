8b99088a1086a17601fec1bafadcceec49108447
Akash G <dragon.abhi8@gmail.com>
2022-10-14 14:54:28 +0530

    Completed

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import { ProjectInput } from "./components/project-input"
-import { ProjectList } from "./components/project-list" 
+import { ProjectList } from "./components/project-list"
 import _ from "lodash"
+
+declare var GLOBAL: any  // 
 
 
 new ProjectInput()
@@ -8,8 +10,9 @@ new ProjectInput()
 new ProjectList('active')
 new ProjectList('finished')
 
-console.log(_.shuffle([1,2,3]))
+console.log(_.shuffle([1, 2, 3]))
 
+console.log(GLOBAL)
 
 
 
