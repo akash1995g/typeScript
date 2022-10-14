b39399ebdf311ab8d8f91860c65386ea4944e431
Akash G <dragon.abhi8@gmail.com>
2022-10-14 14:13:03 +0530

    Used older version modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 var path = require("path")
 module.exports = {
+    mode:"none",
     entry: './src/app.ts',
     output : {
         // filename: "bundle.[contenthash].js", // for create bundle hash for cache 
         filename: "bundle.js", // can be any name
-        path: path.resolve(__dirname,"dist")
+        path: path.resolve(__dirname,"dist"),
+        publicPath: '/dist/' // for webpack dev server
+
     },
     devtool:"inline-source-map",
     module:{
