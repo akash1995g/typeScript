3d8b78fbed8a3c8c10baff4d8130fd5067fa2d96
Akash G <dragon.abhi8@gmail.com>
2022-10-14 22:58:05 +0530

    created i/o function with callback

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
+import NewTodo from './components/NewTodo';
+import TodoList from './components/TodoList';
+
 
 var App: React.FC = () => {
+  var todo = [{ id: "t1", text: "react course" }]
+  var [todoList, setTodoList] = useState([])
+
+  const setValue = (enterValue: string) => {
+    var data = {id: Math.random.toString(),text: enterValue}
+   // setTodoList([data])
+  }
+
   return (
     <div className="App">
-      llllll
+      <NewTodo update={setValue} />
+      <TodoList items={todo} />
     </div>
   );
 }
---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,32 @@
+import React ,{useRef} from "react";
+
+// interface NewTodoProps{ // interface or type can be used
+//     update : (enteredText:string) => void
+// }
+
+type NewTodoProps = {
+    update : (enteredText:string) => void
+}
+
+const NewTodo: React.FC <NewTodoProps>= props => {
+
+    const textInputRef = useRef<HTMLInputElement>(null)
+
+    const todoSubmitHandler = (e : React.FormEvent) =>{
+        e.preventDefault()
+        const enteredText  = textInputRef.current!.value
+        props.update(enteredText)
+
+    } 
+
+    return (<form onSubmit={todoSubmitHandler}>
+        <div>
+            <label htmlFor="todo-text">New Todo</label>
+            <input type="text" id="todo-text" ref={textInputRef} />
+        </div>
+        <br/>
+        <button type="submit">Add Todo</button>
+    </form>)
+}
+
+export default NewTodo
