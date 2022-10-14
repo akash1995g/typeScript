9e8f0e5cf5d4d779e36e908178091f6e250b7d74
Akash G <dragon.abhi8@gmail.com>
2022-10-14 14:47:17 +0530

    js to ts library support

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { ProjectInput } from "./components/project-input"
-import { ProjectList } from "./components/project-list"
+import { ProjectList } from "./components/project-list" 
+import _ from "lodash"
 
 
 new ProjectInput()
@@ -7,6 +8,7 @@ new ProjectInput()
 new ProjectList('active')
 new ProjectList('finished')
 
+console.log(_.shuffle([1,2,3]))
 
 
 
