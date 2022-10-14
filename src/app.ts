4e857f36da894f79455ac057244a895c7fc9b296
Akash G <dragon.abhi8@gmail.com>
2022-10-14 15:51:50 +0530

    Added claass validator

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 import { plainToClass } from "class-transformer"
+
 import { Product } from "./product.model"
+import { validate } from 'class-validator';
 
 var products = [
     { "name": "oooo", "price": 12.2 },
@@ -10,8 +12,16 @@ var products = [
 var list = plainToClass(Product, products)
 
 
+var prodqq = new Product("ll", 2.3)
 
-// var prod = new Product("jjjj", 2.3)
+validate(prodqq).then(error => {
+    if (error.length > 0) {
+        console.log("error")
+    } else {
+        console.log("ok")
+    }
+})
+
 
 
 for (const prod of list) {
--- src/product.model.ts
+++ src/product.model.ts
@@ -1,5 +1,12 @@
+import { IsNotEmpty, IsNumber, IsPositive } from "class-validator"
+
 export class Product {
+
+    @IsNotEmpty()
     name: string
+
+    @IsNumber()
+    @IsPositive()
     price: number
     constructor(name: string, price: number) {
         this.name = name
