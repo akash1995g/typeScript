4002dcaeb78fe51baf08b2dd6903aaeae021357e
Akash G <dragon.abhi8@gmail.com>
2022-10-15 00:12:40 +0530

    created node routes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import express from 'express'
 
+import todoRouter from './routes/todos'
 
 var app = express()
+
+app.use("/todos", todoRouter)
+
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.status(500).json({ message: err.message })
+})
 app.listen(3000)
